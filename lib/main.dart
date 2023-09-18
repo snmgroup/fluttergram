97065d48b5a0c93f1864e5576f92503a88e1c20f
Sanim Shrestha <sanimshrestha.60@gmail.com>
2023-09-18 14:18:03 +0545

    main branch

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import "package:flutter_gram/utils/fluttergram.dart";
 import "utils/exporter.dart";
 
 void main() async {
+  //Main Branch
   await FlutterGram.init();
   runApp(const ProviderScope(child: FlutterGramApp()));
 }
